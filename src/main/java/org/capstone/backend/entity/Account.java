77f99012f7c26ae2ed2e35b23eabec8b71d3f9b7
cuong <cuonglthe173588@fpt.edu.vn>
2025-03-02 22:24:16 +0700

    GOBE-2 : fix login google and security

--- src/main/java/org/capstone/backend/entity/Account.java
+++ src/main/java/org/capstone/backend/entity/Account.java
@@ -15,9 +15,10 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
+
     private String email;
 
+    @Column(unique = true, nullable = false)
     private String username;
 
     private String password;
