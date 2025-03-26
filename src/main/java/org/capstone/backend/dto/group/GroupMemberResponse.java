5b686e4df2a4e4c4af8677ce77dd204b4eee9921
AvDuy <avandduy@gmail.com>
2025-03-26 16:29:07 +0700

    GOBE-62 Group feature 1.0.5 Group Detail and Member Information

--- src/main/java/org/capstone/backend/dto/group/GroupMemberResponse.java
+++ src/main/java/org/capstone/backend/dto/group/GroupMemberResponse.java
@@ -3,10 +3,15 @@ package org.capstone.backend.dto.group;
 import lombok.Data;
 import org.capstone.backend.utils.enums.GroupMemberStatus;
 
+import java.time.LocalDateTime;
+
 @Data
 public class GroupMemberResponse {
     private Long id;
     private Long memberId; // 🔥 Only return the ID instead of the whole Member entity
     private String role;
     private GroupMemberStatus status;
+    private String imageUrl;
+    private LocalDateTime joinDate;
+    private String memberName;
 }
