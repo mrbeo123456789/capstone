0fbe773eb835965c27ff417e9566483d4290673d
cuong <cuonglthe173588@fpt.edu.vn>
2025-04-19 22:11:37 +0700

    fix groups

--- src/main/java/org/capstone/backend/controller/member/MemberController.java
+++ src/main/java/org/capstone/backend/controller/member/MemberController.java
@@ -3,20 +3,25 @@ package org.capstone.backend.controller.member;
 import org.capstone.backend.dto.member.ChangePasswordRequest;
 import org.capstone.backend.dto.member.UserProfileRequest;
 import org.capstone.backend.dto.member.UserProfileResponse;
+import org.capstone.backend.service.auth.AuthService;
 import org.capstone.backend.service.member.MemberService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/member")
 public class MemberController {
 
     private final MemberService memberService;
-
-    public MemberController(MemberService memberService) {
+ private  final AuthService authService;
+    public MemberController(MemberService memberService, AuthService authService) {
         this.memberService = memberService;
+        this.authService = authService;
     }
 
     @GetMapping("/profile")
@@ -38,5 +43,14 @@ public class MemberController {
     public ResponseEntity<String> changePassword(@RequestBody ChangePasswordRequest request) {
         memberService.changePassword(request);
         return ResponseEntity.ok("Password changed successfully!");
+    }
+    @GetMapping("/me")
+    public ResponseEntity<Map<String, Long>> getCurrentMemberId() {
+        Long memberId = authService.getMemberIdFromAuthentication();
+
+
+        Map<String, Long> response = new HashMap<>();
+        response.put("memberId", memberId);
+        return ResponseEntity.ok(response);
     }
 }
